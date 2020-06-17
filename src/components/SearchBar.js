4236e32101ad6c8750902919fa0edfc3daceca13
Ashley <mayashley052@gmail.com>
2020-06-17 17:46:01 -0400

    controlled form for search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
+class SearchBar extends Component {
+  // using state and examples in <input> are examples of controlled form
+  state = { term: "" };
 
-class SearchBar extends Component{
-// method, with event
-onInputChange(e){
-console.log(e.target.value);
-}
+  //   keep form from automaticly submitting itself and causing re-render
+//   we can use these arrow functions for handlers, or we can use it in
+// line handler arrow function like on line 24 onChange, either way is fine
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.term);
+  }
 
-
-
-render(){
-    return(
-        <div className='ui segment'>
-            <form className='ui form'>
-                <div className='field'>
-                    <label>Image Search</label>
-                <input type='text' onChange={this.onInputChange}/>
-                </div>
-            </form>
-        </div>
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
     );
-}
+  }
 }
 export default SearchBar;
+// controlled forms-
+// un-controlled forms-
